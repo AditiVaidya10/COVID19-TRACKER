42533c139f5d153bb4a67397146987461e3b888c
Aditi Atul Vaidya <61463717+AditiTheCoder@users.noreply.github.com>
2021-04-27 20:31:39 +0530

    Add files via upload

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="grid md:grid-cols-2 gap-4">
+    <!-- Box 1 -->
+    <div class="shadow-md bg-red-100 p-12 text-center rounded">
+      <h3 class="text-3xl text-white-900 font-bold mb-6">Cases</h3>
+
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ numberWithCommas(stats.NewConfirmed) }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ numberWithCommas(stats.TotalConfirmed) }}
+      </div>
+    </div>
+
+    <!-- Box 2 -->
+    <div class="shadow-md bg-red-200 p-12 text-center rounded">
+      <h3 class="text-3xl text-white-900 font-bold mb-6">Deaths</h3>
+
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ numberWithCommas(stats.NewDeaths) }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ numberWithCommas(stats.TotalDeaths) }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataBoxes',
+  props: ['stats'],
+  methods: {
+    numberWithCommas(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    },
+  },
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="text-center bg-red-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i> COVID-19 TRACKER
+    </div>
+
+    <p>
+      API POWERED BY :- 
+      <a class="text-white-300" href="https://covid19api.com" target="_blank"
+        >covid19api.com</a
+      >
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
